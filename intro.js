3e4c16c7026ec18f557ac1711072cbba5dd69358
henryz2000 <hzheng3@stuy.edu>
2017-12-08 14:42:30 -0500

    finished

--- intro.js
+++ intro.js
@@ -1,9 +1,37 @@
-var buttonCallback = function() {
+var itemNum = 8;
+var list = document.getElementById("thelist");
+var items = document.getElementsByTagName("li");
+var heading = document.getElementById("h");
+
+var buttonCallback = function(e) {
     var newElement = document.createElement("li");
-    newElement.innerHTML = "test";
-    var list = document.getElementById("thelist");
+    newElement.innerHTML = "item " + itemNum;
+    addListListeners(newElement);
     list.appendChild(newElement);
+    itemNum++;
 }
 
 var button = document.getElementById("b");
 button.addEventListener("click", buttonCallback);
+
+var listEnterCallback = function(e) {
+    heading.innerHTML = this.innerHTML;
+}
+
+var listLeaveCallback = function(e) {
+    heading.innerHTML = "Hello World!";
+}
+
+var listClickCallback = function(e) {
+    this.remove();
+}
+
+var addListListeners = function(item) {
+    item.addEventListener("mouseenter", listEnterCallback);
+    item.addEventListener("mouseleave", listLeaveCallback);
+    item.addEventListener("click", listClickCallback);
+}
+
+for (var i = 0; i < items.length; i++){
+    addListListeners(items[i]);
+}
